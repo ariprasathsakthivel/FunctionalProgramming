be1867415f109825163ca115e3aade4073cf94c6
ariprasathsakthivel <ariprasath3@gmail.com>
2021-07-03 21:05:14 +0530

    added: flipcoin, leapyearcheck

---
+++ FlipCoin/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a positive number for number of times to flip a coin");
+            int count = Convert.ToInt32(Console.ReadLine());
+            CoinPercentage(count);
+        }
+        static void CoinPercentage(int count)
+        {
+            double headCount = 0;
+            double tailCount = 0;
+            double countForPercentage;
+            countForPercentage = count;
+            while (count >= 0)
+            {
+                count--;
+                Random random = new Random();
+                int coin = random.Next(0, 2);
+                if (coin == 0)
+                {
+                    tailCount++;
+                }
+                else if (coin == 1)
+                {
+                    headCount++;
+                }
+            }
+            Console.WriteLine("Tail percentage : {0}", (tailCount / countForPercentage) * 100);
+            Console.WriteLine("Head percentage : {0}", (headCount / countForPercentage) * 100);
+        }   
+    }
+}
---
+++ LeapYear/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the year in the format YYYY");
+            int year = Convert.ToInt32(Console.ReadLine());
+            LeapYearCheck(year);
+        }
+        static void LeapYearCheck(int year)
+        {
+            if ((year%400) == 0)
+            {
+                Console.WriteLine(year +"is a Leap year");
+            }
+            else if ((year%100) == 0)
+            {
+                Console.WriteLine(year +"is a non-leap year");
+            }
+            else if ((year%4) == 0)
+            {
+                Console.WriteLine(year +"is a Leap year");
+            }
+            else
+            {
+                Console.WriteLine(year +"is a non-leap year");
+            }
+        }
+    }
+}
